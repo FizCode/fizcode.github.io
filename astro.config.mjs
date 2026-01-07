19c25ea58408ca3af9f11000fcd1bc0d6f134b7a
fizcode <phintracoaideveloper@gmail.com>
2026-01-07 23:44:36 +0700

    [FIX] Remove some comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // MENGGANTI INI: Ganti dengan URL GitHub Pages Anda (contoh: https://fizcode.github.io)
     site: 'https://fizcode.github.io',
-    // Jika repository Anda bukan root (misal: https://fizcode.github.io/my-portfolio), set base ke '/my-portfolio'
     // base: '/intro-to-astro',
 });
